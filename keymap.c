167a907e718e83fae9522df5a158f1313d8618bc
Stian <stian108@gmail.com>
2021-06-07 12:43:23 +0200

    Flipped volume.

--- keymap.c
+++ keymap.c
@@ -236,9 +236,9 @@ void oled_task_user(void) {
 void encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
             tap_code(KC_VOLD);
+        } else {
+            tap_code(KC_VOLU);
         }
     } else if (index == 1) {
         if (clockwise) {
